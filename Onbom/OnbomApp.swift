28f36cafa57f2ee21f4f90a94423bacb3e6328e0
Moon <59304977+mun9769@users.noreply.github.com>
2023-09-22 14:43:43 +0900

    Initial Commit

---
+++ Onbom/OnbomApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnbomApp.swift
+//  Onbom
+//
+//  Created by moon on 2023/09/22.
+//
+
+import SwiftUI
+
+@main
+struct OnbomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
