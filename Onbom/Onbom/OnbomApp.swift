c619e21fcafb4e422992f5ddd7441732cf4e520c
Moon <59304977+mun9769@users.noreply.github.com>
2023-09-24 17:19:51 +0900

    [Setting/#5] firebase package add
    파이어베이스 auth와 firestore를 사용하기 위해 package를 추가합니다

--- Onbom/Onbom/OnbomApp.swift
+++ Onbom/Onbom/OnbomApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct OnbomApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
