3fe352fec43fe15f3b40eafe08c1ccef40f702eb
Moon <59304977+mun9769@users.noreply.github.com>
2023-09-24 18:37:37 +0900

    [Setting/#5] apple login implement
    ios14부터 지원하는 `SignInWithAppleButton`를 이용하여 애플로그인을 구현한다
    xcode의 capability에 "sign in with apple"을 추가한다

---
+++ Onbom/Onbom/Core/AuthenticationViewModel.swift
@@ -0,0 +1,105 @@
+//
+//  AuthenticationViewModel.swift
+//  Onbom
+//
+//  Created by moon on 2023/09/24.
+//
+
+import Foundation
+import FirebaseAuth
+import AuthenticationServices
+import CryptoKit
+
+class AuthenticationViewModel {
+    private var currentNonce: String?
+    private var errorMessage: String?
+    
+    func handleSignInWithAppleRequest(_ request: ASAuthorizationAppleIDRequest) {
+        request.requestedScopes = [.fullName, .email]
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        request.nonce = sha256(nonce)
+    }
+    func handleSignInWithAppleCompletion(_ result: Result<ASAuthorization, Error>) {
+        if case .failure(let failure) = result {
+            errorMessage = failure.localizedDescription
+        }
+        else if case .success(let success) = result {
+            if let appleIdCredential = success.credential as? ASAuthorizationAppleIDCredential {
+                guard let nonce = currentNonce else {
+                    fatalError("Invalid state: a login callback was received, but no login request was sent.")
+                }
+                guard let appleIDToken = appleIdCredential.identityToken else {
+                    print("Unable to fetch identity token.")
+                    return
+                }
+                guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                    print("Unable to serialise token string from data: \(appleIDToken.debugDescription)")
+                    return
+                }
+                let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                          idToken: idTokenString,
+                                                          rawNonce: nonce)
+                
+                Task {
+                    do {
+                        try await Auth.auth().signIn(with: credential)
+                    }
+                    catch {
+                        print("Error authentication: \(error.localizedDescription)")
+                    }
+                }
+            }
+        }
+    }
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+        }
+        catch {
+            print(error)
+            errorMessage = error.localizedDescription
+        }
+    }
+    func deleteAccount() async -> Bool {
+        do {
+            try await Auth.auth().currentUser?.delete()
+            return true
+        }
+        catch {
+            errorMessage = error.localizedDescription
+            return false
+        }
+    }
+    
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        var randomBytes = [UInt8](repeating: 0, count: length)
+        let errorCode = SecRandomCopyBytes(kSecRandomDefault, randomBytes.count, &randomBytes)
+        if errorCode != errSecSuccess {
+            fatalError(
+                "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+            )
+        }
+        
+        let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        
+        let nonce = randomBytes.map { byte in
+            // Pick a random character from the set, wrapping around if needed.
+            charset[Int(byte) % charset.count]
+        }
+        
+        return String(nonce)
+    }
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            String(format: "%02x", $0)
+        }.joined()
+        
+        return hashString
+    }
+    
+}
---
+++ Onbom/Onbom/View/LoginView.swift
@@ -0,0 +1,32 @@
+//
+//  LoginView.swift
+//  Onbom
+//
+//  Created by moon on 2023/09/24.
+//
+
+import SwiftUI
+import _AuthenticationServices_SwiftUI
+
+struct LoginView: View {
+    let viewModel = AuthenticationViewModel()
+    
+    var body: some View {
+        VStack{
+            SignInWithAppleButton{ request in
+                viewModel.handleSignInWithAppleRequest(request)
+            } onCompletion: { result in
+                viewModel.handleSignInWithAppleCompletion(result)
+            }
+            .signInWithAppleButtonStyle(.black)
+            .frame(maxWidth: .infinity, maxHeight: 50)
+            .cornerRadius(8)
+            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+            
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
