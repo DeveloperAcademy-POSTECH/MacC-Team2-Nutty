f85e58421f56a9715e7afd7d81b558427974275e
SeBin-Kwon <sbkwon98@gmail.com>
2023-10-02 17:55:14 +0900

    [Feat/#13] Update PDFViewer

--- Onbom/Onbom/Core/PDFViewer.swift
+++ Onbom/Onbom/Core/PDFViewer.swift
@@ -9,23 +9,21 @@ import SwiftUI
 import PDFKit
 
 struct PDFViewer: UIViewRepresentable {
-//    let documentURL: URL
     var pdfData: Data?
-    
-//    init(documentURL: URL) {
-//        self.documentURL = documentURL
-//    }
+
     func makeUIView(context: Context) -> some UIView {
         let pdfView = PDFView()
         
         if let pdfData {
-//            pdfView.document = PDFDocument(url: self.documentURL)
             pdfView.document = PDFDocument(data: pdfData)
-            pdfView.autoScales = true
-//            pdfView.displayDirection = .horizontal
-            pdfView.minScaleFactor = 0.6
-            pdfView.maxScaleFactor = 5.0
+        } else {
+            pdfView.document = PDFDocument(url: LTCIFormResource)
         }
+        // TODO: pdfData로 넘긴 pdfView만 적용 안되는 이슈 존재
+        pdfView.autoScales = true
+        pdfView.displayDirection = .horizontal
+        pdfView.minScaleFactor = 0.6
+        pdfView.maxScaleFactor = 5.0
         
         return pdfView
     }
@@ -36,10 +34,4 @@ struct PDFViewer: UIViewRepresentable {
             }
     }
 }
-//
-//struct PDFViewer_Previews: PreviewProvider {
-//    static var previews: some View {
-//        PDFViewer(documentURL: formURL)
-//    }
-//}
 
