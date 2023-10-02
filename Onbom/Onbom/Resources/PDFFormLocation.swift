c52e71e43b46c276bb96385efa096348973647e0
SeBin-Kwon <sbkwon98@gmail.com>
2023-10-02 17:00:49 +0900

    [Feat/#13] Add PDFFormLocation

---
+++ Onbom/Onbom/Resources/PDFFormLocation.swift
@@ -0,0 +1,10 @@
+//
+//  PDFFormLocation.swift
+//  Onbom
+//
+//  Created by Sebin Kwon on 2023/10/02.
+//
+
+import Foundation
+
+let LTCIFormResource = Bundle.main.url(forResource: "LTCIForm", withExtension: "pdf")!
