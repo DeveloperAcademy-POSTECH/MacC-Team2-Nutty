32b25fb0bee72076177b286b6f69df1afe676338
금가경 <gageum0@gmail.com>
2023-10-05 00:43:41 +0900

    [Feat/#15] Get Address from Kakao Postcode service

---
+++ Onbom/Onbom/View/PostCodeView.swift
@@ -0,0 +1,62 @@
+//
+//  PostCodeView.swift
+//  Onbom
+//
+//  Created by 금가경 on 2023/10/04.
+//
+
+import SwiftUI
+import WebKit
+
+struct PostCodeView: UIViewRepresentable {
+    var urlString: String
+    @Binding var isPostCodeViewPresented: Bool
+    @Binding var selectedAddress: String
+    
+    func makeUIView(context: Context) -> WKWebView {
+        // WKUserContentController에 Coordinator를 추가합니다.
+        let userContentController = WKUserContentController()
+        userContentController.add(context.coordinator, name: "callBackHandler")
+
+        // WKWebView의 configuration에 userContentController를 설정합니다.
+        let configuration = WKWebViewConfiguration()
+        configuration.userContentController = userContentController
+        
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        webView.navigationDelegate = context.coordinator
+
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            uiView.load(request)
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, WKNavigationDelegate, WKScriptMessageHandler {
+        var parent: PostCodeView
+        
+        init(_ parent: PostCodeView) {
+            self.parent = parent
+        }
+        
+        func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+            if let data = message.body as? [String: Any], let selectedAddress = data["roadAddress"] as? String {
+                parent.selectedAddress = selectedAddress
+                parent.isPostCodeViewPresented = false
+            }
+        }
+    }
+}
+
+struct PostCodeView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostCodeView(urlString: "https://developeracademy-postech.github.io/MacC-Team2-Nutty/", isPostCodeViewPresented: .constant(false), selectedAddress: .constant(""))
+    }
+}
