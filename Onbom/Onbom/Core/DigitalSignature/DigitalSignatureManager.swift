026e6835066b8385eb88e04b0cc865faacdc07c9
SeBin-Kwon <sbkwon98@gmail.com>
2023-10-04 23:28:16 +0900

    [Feat/#13] Add DigitalSignature Manager and View

---
+++ Onbom/Onbom/Core/DigitalSignature/DigitalSignatureManager.swift
@@ -0,0 +1,20 @@
+//
+//  DigitalSignatureManager.swift
+//  Onbom
+//
+//  Created by Sebin Kwon on 2023/10/04.
+//
+
+import SwiftUI
+
+struct DigitalSignatureManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DigitalSignatureManager_Previews: PreviewProvider {
+    static var previews: some View {
+        DigitalSignatureManager()
+    }
+}
---
+++ Onbom/Onbom/View/DigitalSignature/TempWriteSignatureView.swift
@@ -0,0 +1,20 @@
+//
+//  TempWriteSignatureView.swift
+//  Onbom
+//
+//  Created by Sebin Kwon on 2023/10/04.
+//
+
+import SwiftUI
+
+struct TempWriteSignatureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TempWriteSignatureView_Previews: PreviewProvider {
+    static var previews: some View {
+        TempWriteSignatureView()
+    }
+}
