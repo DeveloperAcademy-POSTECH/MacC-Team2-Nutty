8de6082f02652fe9abf2abcd4f5df3494612e214
Moon <59304977+mun9769@users.noreply.github.com>
2023-10-05 19:32:25 +0900

    [Feat/#5] import GoogleService-Info
    GoogleService-Info를 Resource/ 추가하여 firebase 설정을 구성한다.
    xcode cloud에서 동작할 수 있게 shell script도 작성했다

--- Onbom/Onbom/OnbomApp.swift
+++ Onbom/Onbom/OnbomApp.swift
@@ -13,24 +13,8 @@ import KakaoSDKAuth
 @main
 struct OnbomApp: App {
     
-    func configureOptions() -> FirebaseOptions {
-        let options = FirebaseOptions(googleAppID: "1:349207644054:ios:71f455f29832d34f7a3fb7",
-                                               gcmSenderID: "349207644054")
-        options.apiKey = "AIzaSyARaTo24SMU2pKkzLLFoNld9oTLTIVT7js"
-        options.projectID = "swift-firebase-example-12b92"
-
-        options.bundleID = "Nutty.Onbom"
-        options.databaseURL = "https://myproject.firebaseio.com"
-        options.deepLinkURLScheme = "myapp://"
-        options.storageBucket = "swift-firebase-example-12b92.appspot.com"
-        
-        return options
-    }
-    
     init() {
-        
-        let options = configureOptions()
-        FirebaseApp.configure(options: options)
+        FirebaseApp.configure()
         
         let kakaoAppKey = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"]!
         KakaoSDK.initSDK(appKey: kakaoAppKey as! String)
