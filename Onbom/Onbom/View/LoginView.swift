a84e094cfad541dd8b8f5636cc68d5843ce94cf4
Moon <59304977+mun9769@users.noreply.github.com>
2023-10-05 15:43:54 +0900

    [Chore/#5] indentation after tab width
    xcode 설정에서 tab width를 4에서 2로 줄임

--- Onbom/Onbom/View/LoginView.swift
+++ Onbom/Onbom/View/LoginView.swift
@@ -10,33 +10,33 @@ import _AuthenticationServices_SwiftUI
 import FirebaseAuth
 
 struct LoginView: View {
-  @EnvironmentObject var authViewModel: AuthenticationViewModel
-  
-  var body: some View {
-	VStack{
-	  SignInWithAppleButton{ request in
-		authViewModel.handleSignInWithAppleRequest(request)
-	  } onCompletion: { result in
-		authViewModel.handleSignInWithAppleCompletion(result)
-	  }
-	  .signInWithAppleButtonStyle(.black)
-	  .frame(maxWidth: .infinity, maxHeight: 50)
-	  .cornerRadius(8)
-	  .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
-	  
-	  Button {
-		authViewModel.handleSignInWithKakaoLogin()
-	  } label: {
-		Image("kakao_login_medium_wide")
-		  .resizable()
-		  .aspectRatio(contentMode: .fit)
-	  }
-	  .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
-	  
+	@EnvironmentObject var authViewModel: AuthenticationViewModel
+	
+	var body: some View {
+		VStack{
+			SignInWithAppleButton{ request in
+				authViewModel.handleSignInWithAppleRequest(request)
+			} onCompletion: { result in
+				authViewModel.handleSignInWithAppleCompletion(result)
+			}
+			.signInWithAppleButtonStyle(.black)
+			.frame(maxWidth: .infinity, maxHeight: 50)
+			.cornerRadius(8)
+			.padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+			
+			Button {
+				authViewModel.handleSignInWithKakaoLogin()
+			} label: {
+				Image("kakao_login_medium_wide")
+					.resizable()
+					.aspectRatio(contentMode: .fit)
+			}
+			.padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+			
+		}
 	}
-  }
 }
 
 #Preview {
-  LoginView()
+	LoginView()
 }
