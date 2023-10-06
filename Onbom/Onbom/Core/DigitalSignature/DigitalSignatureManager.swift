78714371eb1e1005a80180dab2adca9386b1fc14
SeBin-Kwon <sbkwon98@gmail.com>
2023-10-06 14:21:39 +0900

    [Feat/#13] Add DigitalSignatureManager

--- Onbom/Onbom/Core/DigitalSignature/DigitalSignatureManager.swift
+++ Onbom/Onbom/Core/DigitalSignature/DigitalSignatureManager.swift
@@ -7,14 +7,47 @@
 
 import SwiftUI
 
-struct DigitalSignatureManager: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class DigitalSignatureManager: ObservableObject {
+    @Published var paths: [[CGPoint]] = []
+    @Published var currentPath: [CGPoint] = []
+    let rectangle = Rectangle().path(in: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width-40, height: 250))
+    
+    func gesture() -> some Gesture {
+        DragGesture()
+            .onChanged { value in
+                if self.rectangle.contains(value.location) {
+                    self.addNewcurrentPath(value)
+                }
+            }
+            .onEnded { value in
+                self.paths.append(self.currentPath)
+                self.currentPath = []
+            }
+    }
+    
+    func addNewcurrentPath(_ value: DragGesture.Value) {
+        currentPath.append(value.location)
+    }
+    
+    func resetPaths() {
+        self.paths = []
+        self.currentPath = []
     }
 }
 
-struct DigitalSignatureManager_Previews: PreviewProvider {
-    static var previews: some View {
-        DigitalSignatureManager()
+//MARK: 전자서명 그리는 선
+struct DrawLine: Shape {
+    var currentPath: [CGPoint]
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        guard let firstPoint = currentPath.first else { return path }
+        
+        path.move(to: firstPoint)
+        for pathIndex in 1..<currentPath.count {
+            path.addLine(to: currentPath[pathIndex])
+        }
+        return path
     }
 }
+
