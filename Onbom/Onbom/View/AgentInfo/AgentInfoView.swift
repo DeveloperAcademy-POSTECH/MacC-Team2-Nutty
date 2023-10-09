ac2b30151482f0971ddd9fe9e592683c6c181bc1
SeBin-Kwon <sbkwon98@gmail.com>
2023-10-10 03:24:35 +0900

    [Feat/#28] Create AgentInfoView and AgentInfoDetailView

---
+++ Onbom/Onbom/View/AgentInfo/AgentInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  AgentInfoView.swift
+//  Onbom
+//
+//  Created by Sebin Kwon on 2023/10/09.
+//
+
+import SwiftUI
+
+struct AgentInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AgentInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        AgentInfoView()
+    }
+}
