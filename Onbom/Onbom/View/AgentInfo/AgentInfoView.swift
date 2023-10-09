259c3700cc7dcde13ac7b027af151c78d7d2aa85
SeBin-Kwon <sbkwon98@gmail.com>
2023-10-10 03:25:13 +0900

    [Feat/#28] Update AgentInfoView design

--- Onbom/Onbom/View/AgentInfo/AgentInfoView.swift
+++ Onbom/Onbom/View/AgentInfo/AgentInfoView.swift
@@ -8,8 +8,67 @@
 import SwiftUI
 
 struct AgentInfoView: View {
+    @State private var isGuardian = false
+    @State private var agentDetailType = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("환자분과의\n상세 관계를 선택해 주세요")
+                    .H1()
+                Spacer()
+            }
+            HStack {
+                Text("보호자이신가요?")
+                    .Label()
+                Spacer()
+            }
+            HStack {
+                Button {
+                    isGuardian = true
+                } label: {
+                    Text("네")
+                        .B3()
+                }
+                Button {
+                    isGuardian = false
+                } label: {
+                    Text("아니요")
+                        .B3()
+                }
+            }
+            HStack {
+                Text("대리인 상세 관계")
+                    .Label()
+                Spacer()
+            }
+            HStack {
+                Button {
+                    agentDetailType = true
+                } label: {
+                    Text("가족")
+                        .B3()
+                }
+                Button {
+                    agentDetailType = false
+                } label: {
+                    Text("친족")
+                        .B3()
+                }
+            }
+            Spacer()
+            Text("상세 관계를 모르겠어요")
+                .Label()
+                .foregroundColor(.G5)
+                .underline()
+            Button {
+                
+            } label: {
+                Text("다음")
+            }
+
+        }
+        .padding(20)
+        
     }
 }
 
