df9daef855ca5bdaba65e6a72e9a0c3fc2110484
Moon <59304977+mun9769@users.noreply.github.com>
2023-10-15 15:11:02 +0900

    [Feat/#51] create HomeNavigationViewModel
    Navigation State를 관리하는 클래스 정의
    HomeView에서 갈 수 있는 화면만 모아서 `HomeRoute` enum을 정의
    모든 뷰가 homeNavigation에 접근할 수 있게끔 environmentObject로 선언해준다.

---
+++ Onbom/Onbom/Core/HomeNavigationViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  HomeNavigationViewModel.swift
+//  Onbom
+//
+//  Created by moon on 10/14/23.
+//
+
+import SwiftUI
+
+class HomeNavigationViewModel: ObservableObject{
+    @Published var homePath: [HomeRoute] = []
+    
+    func navigate(_ route: HomeRoute)  {
+        homePath.append(route)
+    }
+    
+    func pop() {
+        homePath.removeLast()
+    }
+    
+    func popToRoot() {
+        homePath = []
+    }
+}
+
+public enum HomeRoute {
+    case MainView
+    case ApplyTypeView
+    case MediHistoryView
+    case MediConditionView
+    case IDCardDescriptionView
+    case IDCardOCRView
+    case IDCardConfirmEditView
+    case AddressFormView
+    case SignatureView
+    case SubmitCheckListView
+    case StepView_First
+    case StepView_Second
+    case PatientInfoView
+    case AgentInfoView
+    case RejectView
+}
--- Onbom/Onbom/View/MainView.swift
+++ Onbom/Onbom/View/MainView.swift
@@ -8,23 +8,47 @@
 import SwiftUI
 
 struct MainView: View {
-    @State private var navigationPath: [Int] = []
+    @StateObject private var homeNavigation = HomeNavigationViewModel()
     
     var body: some View {
         TabView {
+            NavigationStack(path: $homeNavigation.homePath) {
+                HomeView()
+                    .navigationDestination(for: HomeRoute.self) { route in
+                        switch(route) {
+                        case .ApplyTypeView:                    ApplyTypeView().toolbar(.hidden, for: .tabBar)
+                        case .MediHistoryView:                  MediHistoryView()
+                        case .MediConditionView:                MediConditionView()
+                        case .IDCardDescriptionView:            IDCardDescriptionView()
+                        case .IDCardOCRView:                    IDCardOCRView()
+                        case .IDCardConfirmEditView:            IDCardConfirmEditView(image: .constant(UIImage()))
+                        case .AddressFormView:                  AddressFormView(formType: .actualPatient)
+                        case .SignatureView:                    SignatureView()
+                        case .SubmitCheckListView:              SubmitCheckListView()
+                        case .StepView_First:                   StepView(state: .FIRST)
+                        case .StepView_Second:                  StepView(state: .SECOND)
+                        case .PatientInfoView:                  PatientInfoView()
+                        case .AgentInfoView:                    AgentInfoView()
+                        case .RejectView:                       RejectView()
+                        default:                                RejectView()
+                        }
+                    }
             }
+            .environmentObject(homeNavigation)
             .tint(Color.G5)
             .tabItem {
                 Image("home")
                 Text("홈")
                     .Cap5()
             }
+            
             Text("신청 내역 화면")
             .tabItem {
                 Image("form_history")
                 Text("신청 내역")
                     .Cap5()
             }
+            
             Text("프로필 화면")
             .tabItem {
                 Image("profile")
