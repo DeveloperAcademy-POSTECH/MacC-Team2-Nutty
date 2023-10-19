6e18782cf34e9311dacf2b8d56e6a5ac8c116226
Moon <59304977+mun9769@users.noreply.github.com>
2023-10-20 02:20:08 +0900

    [Fix] delete annotation

--- Onbom/Onbom/OnbomApp.swift
+++ Onbom/Onbom/OnbomApp.swift
@@ -23,12 +23,3 @@ struct OnbomApp: App {
     }
 }
 
-//    .onAppear {
-//        for family: String in UIFont.familyNames {
-//            print (family)
-//            for names : String in
-//                    UIFont.fontNames (forFamilyName: family){
-//                print("=== \(names)")
-//            }
-//        }
-//    }
