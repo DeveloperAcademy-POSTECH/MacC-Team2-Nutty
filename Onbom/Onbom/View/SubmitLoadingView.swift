3d815fa3b363ad81fa23e1833f4f129457a482a3
금가경 <gageum0@gmail.com>
2023-10-23 23:02:06 +0900

    [Design] Create SubmitLoadingView

---
+++ Onbom/Onbom/View/SubmitLoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  SubmitLoadingView.swift
+//  Onbom
+//
+//  Created by 금가경 on 2023/10/23.
+//
+
+import SwiftUI
+
+struct SubmitLoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SubmitLoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SubmitLoadingView()
+    }
+}
