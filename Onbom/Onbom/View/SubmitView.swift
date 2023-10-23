23e45a766e0ab4914d266bc0947f69db10ac54b7
금가경 <gageum0@gmail.com>
2023-10-24 00:56:46 +0900

    [Design] Create SubmitView and SubmitCheckView

--- Onbom/Onbom/View/SubmitView.swift
+++ Onbom/Onbom/View/SubmitView.swift
@@ -9,7 +9,40 @@ import SwiftUI
 
 struct SubmitView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack(spacing: 20) {
+                Text("신청 끝!\n약 2주 후 방문 조사가 있어요")
+                    .H2()
+                    .lineSpacing(10)
+                    .foregroundColor(.B)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 40)
+                Text("어르신 방문을 위해 건강보험공단에서 연락을 할 거예요\n등급 산정 과정에서 추가 서류가 필요할 수도 있어요")
+                    .Cap3()
+                    .lineSpacing(10)
+                    .foregroundColor(.G5)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .frame(maxHeight: .infinity, alignment: .topLeading)
+            .padding(.bottom, 71)
+            Image("SubmitView")
+                .padding(.bottom, 90)
+            VStack(spacing: 0) {
+                CTAButton.CustomButtonView(
+                    style: .primary(isDisabled: false))
+                {
+                    print()
+                } label: {
+                    Text("신청 완료")
+                }
+                .padding(.bottom, 16)
+                Text("신청에 필요한 수수료는 온봄이 냈어요")
+                    .Label()
+                    .foregroundColor(.G4)
+            }
+        }
+        .navigationBarBackButtonHidden(true)
+        .padding([.top, .leading, .trailing], 20.0)
     }
 }
 
