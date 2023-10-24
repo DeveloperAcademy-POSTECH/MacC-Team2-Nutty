6c03b6a890c5eb8f5f079bcf9fc7831dbbe0f36a
금가경 <gageum0@gmail.com>
2023-10-24 11:41:55 +0900

    [Design] Modify RejectView

--- Onbom/Onbom/View/RejectView.swift
+++ Onbom/Onbom/View/RejectView.swift
@@ -16,22 +16,24 @@ struct RejectView: View {
                 .foregroundColor(Color.B)
                 .H2()
                 .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.bottom, 19)
-            Text("최근 입원/수술 이력이 있으면 등급심사를 할 수 없어요")
-                .foregroundColor(Color.G5)
-                .Cap3()
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.bottom, 10)
-            Text("장기요양등급을 신청할 수 있을 때까지 기다리는 동안\n온봄이 몇 가지 서비스를 추천해 드릴게요")
-                .foregroundColor(Color.G5)
-                .Cap3()
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.bottom, 70)
+                .padding(.bottom, 21)
+            VStack {
+                Text("최근 입원/수술 이력이 있으면 등급심사를 할 수 없어요")
+                    .foregroundColor(Color.G5)
+                    .Cap3()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 10)
+                Text("장기요양등급을 신청할 수 있을 때까지 기다리는 동안\n온봄이 몇 가지 서비스를 추천해 드릴게요")
+                    .foregroundColor(Color.G5)
+                    .Cap3()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 70)
+            }
+            .frame(maxWidth:. infinity, minHeight: 75)
                         
             Image("RejectView")
-                
+//                .border(.yellow)
             Spacer()
-            
             CTAButton.CustomButtonView(
                 style: .primary(isDisabled: false))
             {
@@ -39,6 +41,7 @@ struct RejectView: View {
             } label: {
                 Text("한 달 후 다시 알림 받기")
             }
+            .padding(.top, 50)
         }
         .padding([.top, .leading, .trailing], 20)
         .navigationBarBackButton()
