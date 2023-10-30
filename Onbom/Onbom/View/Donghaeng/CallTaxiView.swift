6bc3e72f821921cce397fb9ffcc17af262f7b227
SeBin-Kwon <sbkwon98@gmail.com>
2023-10-30 17:31:31 +0900

    [Chore/#105] Create LTCI folder in View, Move files and Create CallTaxiView in Donghaeng folder

---
+++ Onbom/Onbom/View/Donghaeng/CallTaxiView.swift
@@ -0,0 +1,18 @@
+//
+//  CallTaxiView.swift
+//  Onbom
+//
+//  Created by Sebin Kwon on 10/30/23.
+//
+
+import SwiftUI
+
+struct CallTaxiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CallTaxiView()
+}
