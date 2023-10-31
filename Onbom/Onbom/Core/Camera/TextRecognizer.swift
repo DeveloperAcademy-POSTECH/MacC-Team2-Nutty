0219a27ea81ac35fc9e8384d238563a260abccf9
JJunghyunY <wjdgus995@hanmail.net>
2023-11-01 00:57:16 +0900

    [Feat/#103] Add driver's license ID recognition

--- Onbom/Onbom/Core/Camera/TextRecognizer.swift
+++ Onbom/Onbom/Core/Camera/TextRecognizer.swift
@@ -8,13 +8,71 @@
 import Vision
 import UIKit
 
-class TextRecognizer {
+protocol TextRecognizable {
+    func recognizeText(from image: UIImage, completion: @escaping (String?) -> Void)
+}
+
+class IDCardTextRecognizer: TextRecognizable {
     func recognizeText(from image: UIImage, completion: @escaping (String?) -> Void) {
         guard let cgImage = image.cgImage else {
             completion(nil)
             return
         }
+        
+        let request = VNRecognizeTextRequest { (request, error) in
+            if let error = error {
+                print("IDCardTextRecognizer text recognition error: \(error)")
+                completion(nil)
+                return
+            }
+            guard let observations = request.results as? [VNRecognizedTextObservation] else {
+                completion(nil)
+                return
+            }
+            let recognizedTexts = observations.compactMap { observation in
+                return observation.topCandidates(1).first?.string
+            }
+            let recognizedID = self.filterID(from: recognizedTexts)
+            
+            completion(recognizedID)
+        }
+        
+        request.recognitionLevel = .accurate
+        request.recognitionLanguages = ["ko-KR"]
+        
+        let requests = [request]
+        let imageRequestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+        do {
+            try imageRequestHandler.perform(requests)
+        } catch let error {
+            print("IDCardTextRecognizer VNImageRequestHandler Error: \(error)")
+            completion(nil)
+        }
+    }
+    
+    private func filterID(from texts: [String]) -> String? {
+        var id = ""
+        let idPattern = "\\d{6}\\s?-\\s?\\d{7}"
 
+        for text in texts {
+            if text.range(of: idPattern, options: .regularExpression) != nil {
+                id = text
+                break
+            }
+        }
+        
+        return id.isEmpty ? nil : id
+    }
+}
+
+class DocumentTextRecognizer: TextRecognizable {
+    //나중에 수정할 코드덩어리~
+    func recognizeText(from image: UIImage, completion: @escaping (String?) -> Void) {
+        guard let cgImage = image.cgImage else {
+            completion(nil)
+            return
+        }
+        
         let request = VNRecognizeTextRequest { (request, error) in
             if let error = error {
                 print("TextRecognizer text recognition error: \(error)")
@@ -32,10 +90,10 @@ class TextRecognizer {
             
             completion(recognizedNameID)
         }
-
+        
         request.recognitionLevel = .accurate
         request.recognitionLanguages = ["ko-KR"]
-
+        
         let requests = [request]
         let imageRequestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
         do {
