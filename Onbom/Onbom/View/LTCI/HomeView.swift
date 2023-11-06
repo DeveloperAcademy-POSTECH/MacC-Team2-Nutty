86f5e3e022d63a0f4a1b8d29eb9f084235e8b8a5
Moon <59304977+mun9769@users.noreply.github.com>
2023-11-07 00:44:28 +0900

    [Feat/#124] create LTCICard
    HomeView의 state가 확장될 수 있기 때문에 bool 변수로 관리하지 않고
    enum을 만들어주었다
    
    HomeViewState enum이름 추천좀.. 지금은 ready, apply라고 지음

--- Onbom/Onbom/View/LTCI/HomeView.swift
+++ Onbom/Onbom/View/LTCI/HomeView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct HomeView: View {
+    enum HomeViewState {
+        case ready
+        case apply
+    }
     private let timer = Timer.publish(every: 8, on: .main, in: .common).autoconnect()
     @State private var selectedPage = 0
+    @State private var state: HomeViewState = .ready
     @EnvironmentObject var homeNavigation: HomeNavigationViewModel
     @EnvironmentObject var pdfManager: PDFManager
     let width = UIScreen.main.bounds.width
@@ -54,27 +59,7 @@ struct HomeView: View {
                         selectedPage = (selectedPage + 1) % 2
                     })
                     
-                    VStack(spacing: 30) {
-                        Text("집에서 간편하게\n장기요양등급 신청해 보세요")
-                            .T1()
-                            .foregroundColor(Color.B)
-                            .lineSpacing(4)
-                            .multilineTextAlignment(.center)
-                            .padding(.top, 30)
-                            .frame(maxWidth: .infinity)
-                        Image("MainView")
-                        CTAButton.CustomButtonView(
-                            style: .main)
-                        {
-                            homeNavigation.navigate(.DescriptionView)
-                        } label: {
-                            Text("지금 바로 신청하기")
-                        }
-                        .padding([.horizontal, .bottom], 20)
-                    }
-                    .background(RoundedRectangle(cornerRadius: 20).fill(.white)
-                        .shadow(color: .black.opacity(0.05), radius: 5))
-                    .padding(20)
+                    LTCICard()
                     
                     HStack(spacing: 0) {
                         Text("본인 부담금 계산기")
@@ -156,6 +141,109 @@ struct HomeView: View {
                 }
             }
             .background(Color.G2)
+        }
+    }
+    
+    @ViewBuilder
+    func LTCICard() -> some View {
+        
+        if state == .ready {
+            VStack(spacing: 30) {
+                Text("집에서 간편하게\n장기요양등급 신청해 보세요")
+                    .T1()
+                    .foregroundColor(Color.B)
+                    .lineSpacing(4)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 30)
+                    .frame(maxWidth: .infinity)
+                Image("MainView")
+                CTAButton.CustomButtonView(
+                    style: .main)
+                {
+                    homeNavigation.navigate(.DescriptionView)
+                } label: {
+                    Text("지금 바로 신청하기")
+                }
+                .padding([.horizontal, .bottom], 20)
+            }
+            .background(RoundedRectangle(cornerRadius: 20).fill(.white)
+                .shadow(color: .black.opacity(0.05), radius: 5))
+            .padding(20)
+        }
+        else if state == .apply {
+            VStack(spacing: 0) {
+                HStack(alignment: .center){
+                    Text("장기요양등급신청")
+                        .bold()
+                    Spacer()
+                    Image(systemName: "chevron.forward")
+                        .foregroundColor(Color.G4)
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal, 20)
+                .padding(.top, 24)
+                .padding(.bottom, 12)
+                
+                HStack{
+                    Text("신규 신청")
+                        .foregroundStyle(Color.white)
+                        .font(.system(size: 11, weight: .regular))
+                        .padding(.vertical, 4)
+                        .padding(.horizontal, 6)
+                        .background(RoundedRectangle(cornerRadius: 5).fill(Color.Green4))
+                    Spacer()
+                }
+                .padding(.horizontal, 20)
+                .padding(.bottom, 24)
+                
+                Rectangle()
+                    .fill(Color.G3)
+                    .frame(height: 98)
+                    .padding(.horizontal, 20)
+                    .padding(.bottom, 24)
+                ZStack(alignment: .leading) {
+                    GeometryReader { geometry in
+                        Capsule()
+                            .fill(Color.G2)
+                            .frame(height: 6.5)
+                            .frame(maxWidth: .infinity)
+                        Capsule()
+                            .fill(Color.Green4)
+                            .frame(height: 6.5)
+                            .frame(maxWidth: geometry.size.width / 2)
+                    }
+                }
+                .padding(.horizontal, 20)
+                .padding(.bottom, 7)
+                
+                HStack {
+                    Text("서류 전송")
+                        .font(.system(size: 11))
+                    Spacer()
+                    Text("국민 건강보험공단 확인중")
+                        .font(.system(size: 11))
+                    Spacer()
+                    Text("방문조사")
+                        .font(.system(size: 11))
+                }
+                .padding(.horizontal, 20)
+                .padding(.bottom, 24)
+                
+                Text("국민건강보험공단에서 서류를 접수하면")
+                    .multilineTextAlignment(.center)
+                HStack(spacing: 0){
+                    Text("어르신이 계시는 주소로 방문조사")
+                        .bold()
+                    Text("가 진행돼요")
+                }
+                .padding(.bottom, 24)
+            }
+            .background(RoundedRectangle(cornerRadius: 20).fill(.white)
+                .shadow(color: .black.opacity(0.05), radius: 5))
+            .padding(20)
+            .onTapGesture {
+                homeNavigation.navigate(.ApplyHistoryView)
+            }
         }
     }
 }
