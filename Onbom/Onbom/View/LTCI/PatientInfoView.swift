f16d4248865dd0d6a2c46e813d9640a75e0982c6
Moon <59304977+mun9769@users.noreply.github.com>
2023-11-09 01:21:29 +0900

    [Chore] todo: transfer viewbuilder in patientinfoview

--- Onbom/Onbom/View/LTCI/PatientInfoView.swift
+++ Onbom/Onbom/View/LTCI/PatientInfoView.swift
@@ -29,179 +29,29 @@ struct PatientInfoView: View {
     
     var body: some View {
         VStack(spacing: 0) {
-            Text(step[1] == false ? "어르신의\n성함을 입력해주세요" : viewModel.seniorName + getTitle())
-                .H1()
-                .foregroundColor(Color.B)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal, 20)
-            
-            if(step[2]) {
-                Alert(image: "security", label: "입력한 주민등록번호는 저장되지 않으니 안심하세요")
-                    .padding(.horizontal, 20)
-                    .padding(.bottom, 16)
-                    .padding(.top, 20)
-                    .appear(didAppear[2])
-            }
+            header
             
             ScrollView(showsIndicators: false) {
-                if(step[2]) {
-                    
-                    VStack(spacing: 8) {
-                        Text("주민번호")
-                            .foregroundColor(viewModel.isSeniorIDNumber1Wrong ? Color.R :
-                                                focusedField == .seniorIDNumber1 || focusedField == .seniorIDNumber2 ? Color.Green4 : Color.G6)
-                            .Label()
-                            .padding(.top, 32)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                        
-                        HStack(spacing: 0){
-                            ZStack(alignment: .trailing){
-                                TextField("앞 6자리", text: $viewModel.seniorIDNumber1)
-                                    .font(.custom("Pretendard-Medium", size: 16))
-                                    .lineSpacing(16 / 2 * (100 - 100)/100)
-                                    .kerning(-3/10)
-                                    .foregroundColor(Color.B)
-                                    .onReceive(Just(viewModel.seniorIDNumber1)) { _ in
-                                        if viewModel.seniorIDNumber1.count > 6 {
-                                            viewModel.seniorIDNumber1 = String(viewModel.seniorIDNumber1.prefix(6))
-                                        }
-                                    }
-                                    .onChange(of: viewModel.seniorIDNumber1) { newValue in
-                                        if newValue.count == 6 {
-                                            focusedField = .seniorIDNumber2
-                                        }
-                                    }
-                                    .focused($focusedField, equals: .seniorIDNumber1)
-                                    .keyboardType(.numberPad)
-                                    .padding(16)
-                                    .background(RoundedRectangle(cornerRadius: 10)
-                                        .stroke(viewModel.isSeniorIDNumber1Wrong ? Color.R :
-                                                    focusedField == .seniorIDNumber1 ? Color.Green4 : Color.Green1, lineWidth: 1.5))
-                                    .tint(Color.Green4)
-                                if(viewModel.isSeniorIDNumber1Wrong) {
-                                    Image("wrongInputField")
-                                        .padding(.trailing, 16)
-                                }
-                            }
-                            Text("-")
-                                .padding(.horizontal, 7)
-                            
-                            SecureField("뒤 7자리", text: $viewModel.seniorIDNumber2)
-                                .frame(height:23)
-                                .font(.system(size: 16))
-                                .onReceive(Just(viewModel.seniorIDNumber2)) { _ in
-                                    if viewModel.seniorIDNumber2.count > 7 {
-                                        viewModel.seniorIDNumber2 = String(viewModel.seniorIDNumber2.prefix(7))
-                                    }
-                                }
-                                .onChange(of: viewModel.seniorIDNumber2) { newValue in
-                                    if newValue.count == 7 {
-                                        didFinishTypingAll()
-                                    }
-                                }
-                                .focused($focusedField, equals: .seniorIDNumber2)
-                                .keyboardType(.numberPad)
-                                .padding(16)
-                                .background(RoundedRectangle(cornerRadius: 10)
-                                    .stroke(focusedField == .seniorIDNumber2 ? Color.Green4 : Color.Green1, lineWidth: 1.5))
-                        }
-                    }
-                    .padding(.horizontal, 20)
-                    .appear(didAppear[2])
-                }
-                if(step[1] == true) {
-                    VStack(spacing: 12){
-                        FormTextField(formSubject: "전화번호", placeHolder: "전화번호", textInput: $viewModel.seniorPhoneNumber, isWrong: $viewModel.isSeniorPhoneNumberWrong)
-                            .onReceive(Just(viewModel.seniorPhoneNumber)) { _ in
-                                if viewModel.seniorPhoneNumber.count > 11 {
-                                    viewModel.seniorPhoneNumber = String(viewModel.seniorPhoneNumber.prefix(11))
-                                }
-                            }
-                            .keyboardType(.numberPad)
-                            .focused($focusedField, equals: .seniorPhoneNumber)
-                            .onSubmit {
-                                didFinishTypingName()
-                            }
-                            .disabled(!viewModel.hasMobile)
-                        HStack(spacing: 0){
-                            Image(systemName: "checkmark.circle.fill")
-                                .foregroundColor(viewModel.hasMobile ? Color.G3 : Color.Green4)
-                                .frame(width: 20, height: 20)
-                                .padding(.trailing, 10)
-                            Text("전화번호가 없어요")
-                                .Cap2()
-                                .foregroundColor(.B)
-                        }
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .onTapGesture {
-                            if(viewModel.hasMobile) {
-                                // 핸드폰이 없다고 체크함
-                                viewModel.seniorPhoneNumber = ""
-                                didFinishTypingPhoneNumber()
-                            } else {
-                                // 핸드폰이 있다고 체크함
-                                focusedField = .seniorPhoneNumber
-                            }
-                            viewModel.hasMobile.toggle()
-                        }
-                    }
-                    .padding(.top, 36)
-                    .padding(.horizontal, 20)
-                    .animation(.easeInOut, value: step)
-                    .appear(didAppear[1])
-                }
-                FormTextField(formSubject: "어르신 성함", placeHolder: "성함", textInput: $viewModel.seniorName, isWrong: $viewModel.isSeniorNameWrong)
-                    .onReceive(Just(viewModel.seniorName)) { _ in
-                        if viewModel.seniorName.count > 6 {
-                            viewModel.seniorName = String(viewModel.seniorName.prefix(6))
-                        }
-                    }
-                    .animation(.easeInOut, value: step)
-                    .focused($focusedField, equals: .seniorName)
-                    .onSubmit {
-                        if(viewModel.seniorName.isEmpty) { return }
-                        didFinishTypingName()
-                    }
-                    .padding(.horizontal, 20)
-                    .padding(.top, 36)
-                    .padding(.bottom, 60)
-                // TODO: viewbuilder로 변경
+                if(step[2]) { idNumberField }
+                if(step[1]) { phoneNumberField }
+                if(step[0]) { nameField }
             }
-            if isKeyboardVisible {
-                CTAButton.CustomButtonView(style: .expanded(isDisabled: !isActiveButton())) {
-                    onClickButton()
-                } label: {
-                    Text("다음")
-                }
-                .ignoresSafeArea(.keyboard, edges: .bottom)
-            }
-            // TODO: switch 문으로 변경하기
-            else {
-                CTAButton.CustomButtonView(style: .primary(isDisabled: !isActiveButton())) {
-                    onClickButton()
-                } label: {
-                    Text("다음")
-                }
-                .ignoresSafeArea(.keyboard, edges: .bottom)
-                .padding(.horizontal, 20)
-            }
+            nextButton
         }
-        .onTapGesture {
-            hideKeyboard()
-        }
-        .padding(.top, 20)
-        .onAppear {
-            NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { notification in
-                self.isKeyboardVisible = true
-            }
-            NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { notification in
-                self.isKeyboardVisible = false
-            }
-        }
+        .onAppear { self.onAppear() }
+        .onTapGesture { hideKeyboard() }
         .navigationBarBackButton()
-        .onAppear {
-            focusedField = .seniorName
+    }
+    
+    private func onAppear() {
+        focusedField = .seniorName
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { notification in
+            self.isKeyboardVisible = true
         }
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { notification in
+            self.isKeyboardVisible = false
+        }
+        
     }
     
     private func isActiveButton() -> Bool {
@@ -267,6 +117,174 @@ struct PatientInfoView: View {
     }
 }
 
+
+extension PatientInfoView {
+    private var header: some View {
+        VStack(spacing: 0) {
+            Text(step[1] == false ? "어르신의\n성함을 입력해주세요" : viewModel.seniorName + getTitle())
+                .H1()
+                .foregroundColor(Color.B)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 20)
+                .padding(.top, 20)
+            
+            if(step[2]) {
+                Alert(image: "security", label: "입력한 주민등록번호는 저장되지 않으니 안심하세요")
+                    .padding(.horizontal, 20)
+                    .padding(.bottom, 16)
+                    .padding(.top, 20)
+                    .appear(didAppear[2])
+            }
+        }
+    }
+    
+    private var nameField : some View {
+        FormTextField(formSubject: "어르신 성함", placeHolder: "성함", textInput: $viewModel.seniorName, isWrong: $viewModel.isSeniorNameWrong)
+            .onReceive(Just(viewModel.seniorName)) { _ in
+                if viewModel.seniorName.count > 6 {
+                    viewModel.seniorName = String(viewModel.seniorName.prefix(6))
+                }
+            }
+            .animation(.easeInOut, value: step)
+            .focused($focusedField, equals: .seniorName)
+            .onSubmit {
+                if(viewModel.seniorName.isEmpty) { return }
+                didFinishTypingName()
+            }
+            .padding(.horizontal, 20)
+            .padding(.top, 36)
+            .padding(.bottom, 60)
+    }
+    
+    private var phoneNumberField: some View {
+        VStack(spacing: 12){
+            FormTextField(formSubject: "전화번호", placeHolder: "전화번호", textInput: $viewModel.seniorPhoneNumber, isWrong: $viewModel.isSeniorPhoneNumberWrong)
+                .onReceive(Just(viewModel.seniorPhoneNumber)) { _ in
+                    if viewModel.seniorPhoneNumber.count > 11 {
+                        viewModel.seniorPhoneNumber = String(viewModel.seniorPhoneNumber.prefix(11))
+                    }
+                }
+                .keyboardType(.numberPad)
+                .focused($focusedField, equals: .seniorPhoneNumber)
+                .onSubmit {
+                    didFinishTypingName()
+                }
+                .disabled(!viewModel.hasMobile)
+            HStack(spacing: 0){
+                Image(systemName: "checkmark.circle.fill")
+                    .foregroundColor(viewModel.hasMobile ? Color.G3 : Color.Green4)
+                    .frame(width: 20, height: 20)
+                    .padding(.trailing, 10)
+                Text("전화번호가 없어요")
+                    .Cap2()
+                    .foregroundColor(.B)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .onTapGesture {
+                if(viewModel.hasMobile) {
+                    // 핸드폰이 없다고 체크함
+                    viewModel.seniorPhoneNumber = ""
+                    didFinishTypingPhoneNumber()
+                } else {
+                    // 핸드폰이 있다고 체크함
+                    focusedField = .seniorPhoneNumber
+                }
+                viewModel.hasMobile.toggle()
+            }
+        }
+        .padding(.top, 36)
+        .padding(.horizontal, 20)
+        .animation(.easeInOut, value: step)
+        .appear(didAppear[1])
+    }
+    
+    private var idNumberField: some View {
+        VStack(spacing: 8) {
+            Text("주민번호")
+                .foregroundColor(viewModel.isSeniorIDNumber1Wrong ? Color.R :
+                                    focusedField == .seniorIDNumber1 || focusedField == .seniorIDNumber2 ? Color.Green4 : Color.G6)
+                .Label()
+                .padding(.top, 32)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            HStack(spacing: 0){
+                ZStack(alignment: .trailing){
+                    TextField("앞 6자리", text: $viewModel.seniorIDNumber1)
+                        .font(.custom("Pretendard-Medium", size: 16))
+                        .lineSpacing(16 / 2 * (100 - 100)/100)
+                        .kerning(-3/10)
+                        .foregroundColor(Color.B)
+                        .onReceive(Just(viewModel.seniorIDNumber1)) { _ in
+                            if viewModel.seniorIDNumber1.count > 6 {
+                                viewModel.seniorIDNumber1 = String(viewModel.seniorIDNumber1.prefix(6))
+                            }
+                        }
+                        .onChange(of: viewModel.seniorIDNumber1) { newValue in
+                            if newValue.count == 6 {
+                                focusedField = .seniorIDNumber2
+                            }
+                        }
+                        .focused($focusedField, equals: .seniorIDNumber1)
+                        .keyboardType(.numberPad)
+                        .padding(16)
+                        .background(RoundedRectangle(cornerRadius: 10)
+                            .stroke(viewModel.isSeniorIDNumber1Wrong ? Color.R :
+                                        focusedField == .seniorIDNumber1 ? Color.Green4 : Color.Green1, lineWidth: 1.5))
+                        .tint(Color.Green4)
+                    if(viewModel.isSeniorIDNumber1Wrong) {
+                        Image("wrongInputField")
+                            .padding(.trailing, 16)
+                    }
+                }
+                Text("-")
+                    .padding(.horizontal, 7)
+                
+                SecureField("뒤 7자리", text: $viewModel.seniorIDNumber2)
+                    .frame(height:23)
+                    .font(.system(size: 16))
+                    .onReceive(Just(viewModel.seniorIDNumber2)) { _ in
+                        if viewModel.seniorIDNumber2.count > 7 {
+                            viewModel.seniorIDNumber2 = String(viewModel.seniorIDNumber2.prefix(7))
+                        }
+                    }
+                    .onChange(of: viewModel.seniorIDNumber2) { newValue in
+                        if newValue.count == 7 {
+                            didFinishTypingAll()
+                        }
+                    }
+                    .focused($focusedField, equals: .seniorIDNumber2)
+                    .keyboardType(.numberPad)
+                    .padding(16)
+                    .background(RoundedRectangle(cornerRadius: 10)
+                        .stroke(focusedField == .seniorIDNumber2 ? Color.Green4 : Color.Green1, lineWidth: 1.5))
+            }
+        }
+        .padding(.horizontal, 20)
+        .appear(didAppear[2])
+    }
+    
+    // TODO: switch 문으로 변경하기
+    private var nextButton: some View {
+        Group {
+            if isKeyboardVisible {
+                CTAButton.CustomButtonView(style: .expanded(isDisabled: !isActiveButton())) {
+                    onClickButton()
+                } label: {
+                    Text("다음")
+                }
+            }
+            else {
+                CTAButton.CustomButtonView(style: .primary(isDisabled: !isActiveButton())) {
+                    onClickButton()
+                } label: {
+                    Text("다음")
+                }
+                .padding(.horizontal, 20)
+            }
+        }
+        .ignoresSafeArea(.keyboard, edges: .bottom)
+    }
+}
 #Preview {
     PatientInfoView()
         .environmentObject(Patient())
