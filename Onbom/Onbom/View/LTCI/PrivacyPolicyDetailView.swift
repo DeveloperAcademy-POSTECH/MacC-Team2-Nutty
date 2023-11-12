dd3d16512679fbaa3ec467c489437e745253d65a
SeBin-Kwon <sbkwon98@gmail.com>
2023-11-13 04:28:22 +0900

    [Feat/#147] Add WebView

--- Onbom/Onbom/View/LTCI/PrivacyPolicyDetailView.swift
+++ Onbom/Onbom/View/LTCI/PrivacyPolicyDetailView.swift
@@ -6,13 +6,40 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct PrivacyPolicyDetailView: View {
+    @Binding var isShowPrivacyPolicyDetail: Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Button {
+                isShowPrivacyPolicyDetail = false
+            } label: {
+                Image(systemName: "xmark")
+                    .foregroundStyle(Color.G5)
+            }
+            .padding(.horizontal)
+            MyWebView(urlToLoad: "https://lateral-donkey-cf6.notion.site/7e4a8717592040078e844a8f87744a25?pvs=4")
+        }
     }
 }
 
+struct MyWebView: UIViewRepresentable {
+    var urlToLoad: String
+
+    func makeUIView(context: Context) -> WKWebView {
+        guard let url = URL(string: self.urlToLoad) else {
+            return WKWebView()
+        }
+        let webView = WKWebView()
+        webView.load(URLRequest(url: url))
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: UIViewRepresentableContext<MyWebView>) {
+    }
+}
+ 
 #Preview {
-    PrivacyPolicyDetailView()
+    PrivacyPolicyDetailView(isShowPrivacyPolicyDetail: .constant(true))
 }
