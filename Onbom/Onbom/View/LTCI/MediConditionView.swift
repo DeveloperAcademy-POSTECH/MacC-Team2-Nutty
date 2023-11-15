fd54887cad106abc2b1492a5c0c1881e06848006
금가경 <gageum0@gmail.com>
2023-11-15 16:32:26 +0900

    [Design] Delete mental disese item

--- Onbom/Onbom/View/LTCI/MediConditionView.swift
+++ Onbom/Onbom/View/LTCI/MediConditionView.swift
@@ -9,29 +9,27 @@ import SwiftUI
 import Combine
 
 struct MediConditionView: View {
-    @StateObject private var viewModel = MediConditionViewModel()
     @EnvironmentObject var navigation: NavigationManager
     @EnvironmentObject var patient: Patient
+    @State private var hasInfectiousDisease : Bool? = nil
     
     var body: some View {
-        VStack(spacing: 0){
-            Text("어르신이 현재 전염성 질병 또는\n정신질환을 가지고 계신가요?")
-                .H1()
-                .foregroundColor(Color.B)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.bottom, 48)
-            
-            AlternativeForm(formSubject: "전염성 질병 보유 여부", answer: $viewModel.hasInfectiousDisease)
-                .padding(.bottom, 36)
-            AlternativeForm(formSubject: "정신 질환 보유 여부", answer: $viewModel.hasMentalDisorder)
-            
-            Spacer()
-            
+        VStack {
+            ScrollView {
+                Text("어르신이 현재\n전염성 질환을 가지고 계신가요?")
+                    .H1()
+                    .foregroundColor(Color.B)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 48)
+                
+                AlternativeForm(formSubject: "전염성 질환 보유 여부", answer: $hasInfectiousDisease)
+                Spacer()
+            }
             CTAButton.CustomButtonView(
-                style: .primary(isDisabled: !viewModel.formIsValid))
+                style: .primary(isDisabled: hasInfectiousDisease == nil))
             {
-                patient.hasInfectiousDisease = viewModel.hasInfectiousDisease ?? false
-                patient.hasMentalDisorder = viewModel.hasMentalDisorder ?? false
+                guard let hasInfectiousDisease = hasInfectiousDisease else { return }
+                patient.hasInfectiousDisease = hasInfectiousDisease
                 navigation.navigate(.PatientInfoView)
             } label: {
                 Text("다음")
@@ -61,10 +59,10 @@ struct AlternativeForm: View {
                 .padding(.bottom, 4)
             HStack(spacing: 10) {
                 RadioButton.CustomButtonView(style: isCheckedYES() ? .twoSelected : .twoUnselected) {
-                        answer = true
-                    } label: {
-                        Text("예")
-                    }
+                    answer = true
+                } label: {
+                    Text("예")
+                }
                 RadioButton.CustomButtonView(style: isCheckedNO() ? .twoSelected : .twoUnselected) {
                     self.answer = false
                 } label: {
