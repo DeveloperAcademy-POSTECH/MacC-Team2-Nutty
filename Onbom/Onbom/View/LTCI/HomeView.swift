ddfee44bd7a4ac4e0a291be4ce9deef9ea3046c8
Moon <59304977+mun9769@users.noreply.github.com>
2023-11-16 01:54:16 +0900

    [chore/#158] make component in homeview

--- Onbom/Onbom/View/LTCI/HomeView.swift
+++ Onbom/Onbom/View/LTCI/HomeView.swift
@@ -36,33 +36,9 @@ struct HomeView: View {
             }
             ScrollView(showsIndicators: false) {
                 VStack(spacing: 0){
-                    TabView(selection: $selectedPage){
-                        Image("Banner1")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .tag(0)
-                        Image("Banner2")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .tag(1)
-                    }
-                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-                    .frame(width: width, height: 84)
-                    .overlay(
-                        Text("\(selectedPage + 1) / 2")
-                            .foregroundStyle(Color.white)
-                            .font(.system(size: 10, weight: .regular))
-                            .padding(.vertical, 3.5)
-                            .padding(.horizontal, 5)
-                            .background(RoundedRectangle(cornerRadius: 10).fill(Color.TG1))
-                            .offset(x: -18, y: -12),
-                        alignment: .bottomTrailing
-                    )
-                    .onReceive(timer, perform: { _ in
-                        selectedPage = (selectedPage + 1) % 2
-                    })
+                    adBanner
                     
-                    LTCICard()
+                    LTCICard
                     
                     HStack(spacing: 0) {
                         Text("본인 부담금 계산기")
@@ -80,72 +56,15 @@ struct HomeView: View {
                     .padding(.horizontal, 20)
                     .padding(.bottom, 25)
                     
-                    Text("복지정보 한눈에 보기")
-                        .H2()
-                        .foregroundColor(Color.B)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.leading, 20)
-                        .padding(.bottom, 5)
                     
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        LazyHStack(spacing: 0) {
-                            let images = [Image("MainView_Green"),
-                                          Image("MainView_Blue"),
-                                          Image("MainView_Purple"),
-                                          Image("MainView_Yellow"),
-                                          Image("MainView_Pink"),
-                                          Image("MainView_Brown"),]
-                            let colors = [Color.init(hex: "E2F6E2"),
-                                          Color.init(hex: "D6EDF8"),
-                                          Color.init(hex: "DDDEF9"),
-                                          Color.init(hex: "FFFBD4"),
-                                          Color.init(hex: "FFE7F7"),
-                                          Color.init(hex: "DFD6CE"),]
-                            let circleColors = [Color.init(hex: "B4E8B4"),
-                                                Color.init(hex: "9FD4ED"),
-                                                Color.init(hex: "BFC1F6"),
-                                                Color.init(hex: "FAF1A4"),
-                                                Color.init(hex: "FECBED"),
-                                                Color.init(hex: "D6C4B5"),]
-                            let contents = ["치매가족휴가제로\n한 숨 돌려보세요",
-                                            "부담을 덜어드리는\n노인돌봄종합서비스",
-                                            "가장 가까운\n치매상담센터 찾기",
-                                            "필요했던 복지용구\n한번에 신청해보세요",
-                                            "인식표 나눔사업\n여기서 바로 알아보기",
-                                            "장기요양등급,\n1분만에 시설등급 받기",]
-                            
-                            ForEach(0..<6) { i in
-                                VStack(alignment: .leading, spacing: 0) {
-                                    ZStack {
-                                        Circle()
-                                            .fill(circleColors[i])
-                                            .frame(maxWidth: 64, maxHeight: 64)
-                                        images[i]
-                                    }
-                                    .padding(.bottom, 31)
-                                    Text(contents[i])
-                                        .foregroundColor(Color.B)
-                                        .T3()
-                                    Text("알림 설정하기")
-                                        .foregroundColor(Color.G5)
-                                        .Cap4()
-                                        .padding(.top, 5)
-                                }
-                                .padding(20)
-                                .frame(width: 160, height: 194, alignment: .topLeading)
-                                .background(colors[i])
-                                .cornerRadius(12)
-                                .padding(.leading, i == 0 ? 20 : 0)
-                                .padding(.trailing, i == 5 ? 20 : 10)
-                            }
-                        }
-                    }
+                    carouselContent
+                    
                     Spacer().frame(height: 100)
                 }
             }
             .background(Color.G2)
         }
-        .navigationDestination(for: HomeRoute.self) { route in
+        .navigationDestination(for: HomeRoute.self) { route in // TODO: ViewModifier로 빼기
             switch(route) {
             case .OnboardingView:                   OnboardingView(isOnboarding: .constant(true))
             case .ApplyHistoryView:                 ApplyHistoryView()
@@ -176,7 +95,103 @@ struct HomeView: View {
     }
     
     @ViewBuilder
-    func LTCICard() -> some View {
+    private var adBanner: some View {
+        
+        TabView(selection: $selectedPage){
+            Image("Banner1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .tag(0)
+            Image("Banner2")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .tag(1)
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+        .frame(width: width, height: 84)
+        .overlay(
+            Text("\(selectedPage + 1) / 2")
+                .foregroundStyle(Color.white)
+                .font(.system(size: 10, weight: .regular))
+                .padding(.vertical, 3.5)
+                .padding(.horizontal, 5)
+                .background(RoundedRectangle(cornerRadius: 10).fill(Color.TG1))
+                .offset(x: -18, y: -12),
+            alignment: .bottomTrailing
+        )
+        .onReceive(timer, perform: { _ in
+            selectedPage = (selectedPage + 1) % 2
+        })
+    }
+    
+    @ViewBuilder
+    private var carouselContent: some View {
+        
+        VStack(spacing: 0) {
+            Text("복지정보 한눈에 보기")
+                .H2()
+                .foregroundColor(Color.B)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 20)
+                .padding(.bottom, 5)
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack(spacing: 0) {
+                    let images = [Image("MainView_Green"),
+                                  Image("MainView_Blue"),
+                                  Image("MainView_Purple"),
+                                  Image("MainView_Yellow"),
+                                  Image("MainView_Pink"),
+                                  Image("MainView_Brown"),]
+                    let colors = [Color.init(hex: "E2F6E2"),
+                                  Color.init(hex: "D6EDF8"),
+                                  Color.init(hex: "DDDEF9"),
+                                  Color.init(hex: "FFFBD4"),
+                                  Color.init(hex: "FFE7F7"),
+                                  Color.init(hex: "DFD6CE"),]
+                    let circleColors = [Color.init(hex: "B4E8B4"),
+                                        Color.init(hex: "9FD4ED"),
+                                        Color.init(hex: "BFC1F6"),
+                                        Color.init(hex: "FAF1A4"),
+                                        Color.init(hex: "FECBED"),
+                                        Color.init(hex: "D6C4B5"),]
+                    let contents = ["치매가족휴가제로\n한 숨 돌려보세요",
+                                    "부담을 덜어드리는\n노인돌봄종합서비스",
+                                    "가장 가까운\n치매상담센터 찾기",
+                                    "필요했던 복지용구\n한번에 신청해보세요",
+                                    "인식표 나눔사업\n여기서 바로 알아보기",
+                                    "장기요양등급,\n1분만에 시설등급 받기",]
+                    
+                    ForEach(0..<6) { i in
+                        VStack(alignment: .leading, spacing: 0) {
+                            ZStack {
+                                Circle()
+                                    .fill(circleColors[i])
+                                    .frame(maxWidth: 64, maxHeight: 64)
+                                images[i]
+                            }
+                            .padding(.bottom, 31)
+                            Text(contents[i])
+                                .foregroundColor(Color.B)
+                                .T3()
+                            Text("알림 설정하기")
+                                .foregroundColor(Color.G5)
+                                .Cap4()
+                                .padding(.top, 5)
+                        }
+                        .padding(20)
+                        .frame(width: 160, height: 194, alignment: .topLeading)
+                        .background(colors[i])
+                        .cornerRadius(12)
+                        .padding(.leading, i == 0 ? 20 : 0)
+                        .padding(.trailing, i == 5 ? 20 : 10)
+                    }
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private var LTCICard: some View {
         
         if viewModel.state == .ready {
             VStack(spacing: 30) {
@@ -302,5 +317,7 @@ struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView(viewModel: HomeViewModel())
             .environmentObject(NavigationManager())
+            .environmentObject(mockAgent)
+            .environmentObject(mockPatient)
     }
 }
