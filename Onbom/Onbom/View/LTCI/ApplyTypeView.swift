2ac6d00ece9ecba30e2dd119507e30ec90197ffc
금가경 <gageum0@gmail.com>
2023-11-15 16:31:52 +0900

    [Fix] adjust view for dynamic font

--- Onbom/Onbom/View/LTCI/ApplyTypeView.swift
+++ Onbom/Onbom/View/LTCI/ApplyTypeView.swift
@@ -9,66 +9,99 @@ import SwiftUI
 
 struct ApplyTypeView: View {
     @EnvironmentObject var navigation: NavigationManager
+    @State private var isPrivacyPolicyViewPresented = true
+    @State private var selected = 1
+    
     let title: [String] = ["신규 신청", "갱신", "등급 변경"]
     let description: [String] = ["노인성 질병이 있는 경우에만 신청할 수 있어요", "2년에 한 번씩 갱신해 주세요", "환자의 상태가 변했을 때 신청할 수 있어요"]
     
-    @State private var isPrivacyPolicyViewPresented = false
-    
-    @State var oneSelectedIndex : Int = -1
-    
     var body: some View {
         ZStack {
-            VStack {
-                Text("장기요양등급\n신청 종류를 선택해 주세요")
-                    .H1()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.bottom, 48)
-                
-                VStack(spacing: 10) {
-                    ForEach(0..<title.count, id: \.self) { index in
-                        let oneStyle: RadioButtonStyle = oneSelectedIndex == index ? .oneSelected : .oneUnselected
-                        
-                        RadioButton.CustomButtonView(style: oneStyle) {
-                            oneSelectedIndex = index
+            ScrollView {
+                VStack {
+                    Text("장기요양등급\n신청 종류를 선택해 주세요")
+                        .H1()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.bottom, 48)
+                    
+                    VStack(spacing: 10) {
+                        RadioButton.CustomButtonView(style: selected == 1 ? .oneSelected : .oneUnselected) {
+                            selected = 1
                             isPrivacyPolicyViewPresented = true
-                        } label: {
+                        } label : {
                             HStack {
                                 VStack(alignment: .leading, spacing: 13) {
-                                    if index == 0 {
-                                        Text("\(title[index])")
-                                            .T2()
-                                        Text("\(description[index])")
-                                            .Cap3()
-                                            .foregroundColor(.G5)
-                                    }
-                                    else {
-                                        Text("\(title[index])")
-                                            .T2()
-                                            .foregroundColor(.G3)
-                                        Text("\(description[index])")
-                                            .Cap3()
-                                            .foregroundColor(.G3)
-                                    }
+                                    Text("\(title[0])")
+                                        .T2()
+                                        .foregroundStyle(Color.B)
+                                    Text("\(description[0])")
+                                        .Cap3()
+                                        .foregroundStyle(Color.G5)
+                                }
+                                Spacer()
+                            }
+                        }.tag(1)
+                        
+                        
+                        
+                        RadioButton.CustomButtonView(style: selected == 2 ? .oneSelected : .oneUnselected) {
+                            selected = 2
+                        } label : {
+                            HStack {
+                                VStack(alignment: .leading, spacing: 13) {
+                                    Text("\(title[1])")
+                                        .T2()
+                                        .foregroundStyle(Color.G3)
+                                    Text("\(description[1])")
+                                        .Cap3()
+                                        .foregroundStyle(Color.G3)
                                 }
                                 Spacer()
                             }
                         }
-                        .disabled(index > 0)
+                        .tag(2)
+                        .disabled(true)
+                        
+                        RadioButton.CustomButtonView(style: selected == 3 ? .oneSelected : .oneUnselected) {
+                            selected = 3
+                        } label : {
+                            HStack {
+                                VStack(alignment: .leading, spacing: 13) {
+                                    Text("\(title[2])")
+                                        .T2()
+                                        .foregroundStyle(Color.G3)
+                                    Text("\(description[2])")
+                                        .Cap3()
+                                        .foregroundStyle(Color.G3)
+                                }
+                                Spacer()
+                            }
+                        }
+                        .tag(3)
+                        .disabled(true)
                     }
+                    Spacer()
                 }
-                Spacer()
+                .navigationBarBackButton()
+                .padding(20)
+                .sheet(isPresented: $isPrivacyPolicyViewPresented) {
+                    PrivacyPolicyView(isPrivacyPolicyViewPresented: $isPrivacyPolicyViewPresented)
+                        .presentationDetents([.fraction(0.4), .large])
+                        .presentationDragIndicator(.hidden)
+                }
             }
-            .navigationBarBackButton()
-            .padding(20)
-            .sheet(isPresented: $isPrivacyPolicyViewPresented) {
-                PrivacyPolicyView(isPrivacyPolicyViewPresented: $isPrivacyPolicyViewPresented)
-                    .presentationDetents([.fraction(0.4)])
-            }
-            
             if isPrivacyPolicyViewPresented {
                 Color.black.opacity(0.3).ignoresSafeArea()
             }
         }
+    }
+    
+    private func isCheckedYes() -> Bool {
+        return true
+    }
+    
+    private func isCheckedNo() -> Bool {
+        return true
     }
 }
 struct ApplyTypeView_Previews: PreviewProvider {
