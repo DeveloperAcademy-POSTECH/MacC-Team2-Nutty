f29a69977fc62e5b44bb6f8309e33002820a346c
SeBin-Kwon <sbkwon98@gmail.com>
2023-11-23 17:25:56 +0900

    [Design/#185] Update LTCICard Back

--- Onbom/Onbom/Components/LTCICardBack.swift
+++ Onbom/Onbom/Components/LTCICardBack.swift
@@ -36,13 +36,7 @@ struct LTCICardBack: View {
                 Spacer()
             }
             .padding(.horizontal, 20)
-            .padding(.bottom, 24)
-            
-            Rectangle()
-                .fill(Color.G3)
-                .frame(height: 98)
-                .padding(.horizontal, 20)
-                .padding(.bottom, 24)
+            .padding(.bottom, 80)
             ZStack(alignment: .leading) {
                 GeometryReader { geometry in
                     Capsule()
