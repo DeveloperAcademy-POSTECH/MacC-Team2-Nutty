361e1910b18c0b0a454c691e0a94a0f69e42badd
Moon <59304977+mun9769@users.noreply.github.com>
2023-12-01 23:17:17 +0900

    [chore] remove OneSelectedButton
    ButtonStyle을 사용하기 때문에 삭제한다

--- Onbom/Onbom/View/LTCI/ApplyTypeView.swift
+++ Onbom/Onbom/View/LTCI/ApplyTypeView.swift
@@ -7,12 +7,31 @@
 
 import SwiftUI
 
+
+public enum ApplyTypeViewRadioOne: RadioOneProtocol {
+    case new, renew, change
+    
+    var title: String {
+        switch(self) {
+        case .new: return "신규 신청"
+        case .renew: return "갱신"
+        case .change: return "등급 변경"
+        }
+    }
+    
+    var desc: String {
+        switch(self) {
+        case .new: return "노인성 질병이 있는 경우에만 신청할 수 있어요"
+        case .renew: return "2년에 한 번씩 갱신해 주세요"
+        case .change: return "환자의 상태가 변했을 때 신청할 수 있어요"
+        }
+    }
+}
+
 struct ApplyTypeView: View {
     @EnvironmentObject var navigation: NavigationManager
     @State private var isPrivacyPolicyViewPresented = false
-    @State private var selected = 0
-    let title: [String] = ["신규 신청", "갱신", "등급 변경"]
-    let description: [String] = ["노인성 질병이 있는 경우에만 신청할 수 있어요", "2년에 한 번씩 갱신해 주세요", "환자의 상태가 변했을 때 신청할 수 있어요"]
+    @State private var selected: ApplyTypeViewRadioOne? = nil
     
     var body: some View {
         ZStack {
@@ -23,63 +42,12 @@ struct ApplyTypeView: View {
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .padding(.bottom, 48)
                     
-                    VStack(spacing: 10) {
-                        RadioButton.CustomButtonView(style: selected == 1 ? .oneSelected : .oneUnselected) {
-                            selected = 1
-                            isPrivacyPolicyViewPresented = true
-                        } label : {
-                            HStack {
-                                VStack(alignment: .leading, spacing: 13) {
-                                    Text("\(title[0])")
-                                        .T2()
-                                        .foregroundStyle(Color.B)
-                                    Text("\(description[0])")
-                                        .Cap3()
-                                        .foregroundStyle(Color.G5)
-                                }
-                                Spacer()
-                            }
-                        }.tag(1)
-                        
-                        
-                        
-                        RadioButton.CustomButtonView(style: selected == 2 ? .oneSelected : .oneUnselected) {
-                            selected = 2
-                        } label : {
-                            HStack {
-                                VStack(alignment: .leading, spacing: 13) {
-                                    Text("\(title[1])")
-                                        .T2()
-                                        .foregroundStyle(Color.G3)
-                                    Text("\(description[1])")
-                                        .Cap3()
-                                        .foregroundStyle(Color.G3)
-                                }
-                                Spacer()
-                            }
-                        }
-                        .tag(2)
-                        .disabled(true)
-                        
-                        RadioButton.CustomButtonView(style: selected == 3 ? .oneSelected : .oneUnselected) {
-                            selected = 3
-                        } label : {
-                            HStack {
-                                VStack(alignment: .leading, spacing: 13) {
-                                    Text("\(title[2])")
-                                        .T2()
-                                        .foregroundStyle(Color.G3)
-                                    Text("\(description[2])")
-                                        .Cap3()
-                                        .foregroundStyle(Color.G3)
-                                }
-                                Spacer()
-                            }
-                        }
-                        .tag(3)
-                        .disabled(true)
-                    }
-                    Spacer()
+                    Button(action: {}, label: {})
+                        .buttonStyle(RadioOneButtonStyle(type: .new, selected: $selected, action: showModal))
+                    Button(action: {}, label: {})
+                        .buttonStyle(RadioOneButtonStyle(type: .renew, selected: $selected, disabled: true))
+                    Button(action: {}, label: {})
+                        .buttonStyle(RadioOneButtonStyle(type: .change, selected: $selected, disabled: true))
                 }
                 .navigationBarBackButton()
                 .padding(20)
@@ -95,12 +63,8 @@ struct ApplyTypeView: View {
         }
     }
     
-    private func isCheckedYes() -> Bool {
-        return true
-    }
-    
-    private func isCheckedNo() -> Bool {
-        return true
+    func showModal() {
+        isPrivacyPolicyViewPresented = true
     }
 }
 struct ApplyTypeView_Previews: PreviewProvider {
